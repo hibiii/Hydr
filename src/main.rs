7bf6e815b84fefef87e16ee441e5fa7ab34c4ad6
hibi <69405847+Hibiii@users.noreply.github.com>
2023-03-21 14:26:59 -0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,22 @@
+use std::{
+    io::{stdin, stdout, Write},
+    process::exit,
+};
+
+use termion::{color, is_tty, style};
+
+fn main() -> std::io::Result<()> {
+    let mut stdout = stdout();
+    if !is_tty(&stdout) || !is_tty(&stdin()) {
+        eprintln!("hydr requires an interactive terminal");
+        exit(1);
+    }
+    write!(
+        stdout,
+        "Hello, {}Rust{}!\n",
+        color::Fg(color::Rgb(0xff, 0x80, 0x00)),
+        style::Reset,
+    )?;
+
+    Ok(())
+}
